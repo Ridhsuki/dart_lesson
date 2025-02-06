922ce40cd4785d02eba426a4df64d3f58fd21ef0
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-02-06 08:30:58 +0700

    make materials about variables

---
+++ bin/01_variables.dart
@@ -0,0 +1,14 @@
+// Variables and Comments
+
+void main() {
+  //! Declaring Dart Variables
+
+  //? Method #1
+  String var1;
+  var1 = 'This is the value of the var1 variable';
+  print(var1);
+
+  //? Method #2
+  String var2 = 'This is the value of the var2 variable';
+  print(var2);
+}
