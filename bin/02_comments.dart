c6f5e4387d61533a3facdbe03adb6826373144f3
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-02-07 20:25:39 +0700

    Add Comments Materials

---
+++ bin/02_comments.dart
@@ -0,0 +1,15 @@
+// Comments in Dart 
+
+//? Method #1
+// Single Line Comment
+
+//? Method #2
+/*
+Multiline
+Comment
+*/
+
+//? Method #3
+/// Documentation Comments
+/// Documentation Comments
+/// Documentation Comments
