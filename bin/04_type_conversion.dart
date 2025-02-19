04bbc65595801d6134c875ee7dcdc6c46b737bdb
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-02-19 22:36:07 +0700

    add type conversion material (Integer to other)

--- bin/04_type_conversion.dart
+++ bin/04_type_conversion.dart
@@ -6,10 +6,19 @@ void stringToOthers() {
   String originalString = '18';
 
   // string to int
-  print(int.parse(originalString)); // output = 18
+  print(int.parse(originalString)); // output = 18 (int)
 
   // string to int
-  print(double.parse(originalString)); // output = 18.0
+  print(double.parse(originalString)); // output = 18.0 (double)
 }
 
+//* 2.Integer to Other (.toString(), .toDouble())
+void integerToOthers() {
+  int originalInteger = 11;
 
+  // integer to string
+  print(originalInteger.toString()); // output = '1' (string)
+
+  // integer to double
+  print(originalInteger.toDouble()); // output = 11.0 (double)
+}
