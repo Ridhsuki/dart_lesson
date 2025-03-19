cad668aa5dbf3389e1d7b1fd6e047e0ef43c08dc
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-03-19 22:42:01 +0700

    edit to the default code in the file inside of lib folder

--- lib/dart_lesson.dart
+++ lib/dart_lesson.dart
@@ -1,5 +1,3 @@
-/// Lorem ipsum odor amet, consectetuer adipiscing elit. Commodo eget platea mauris mollis sapien. Facilisi pretium a facilisis tortor, cubilia lobortis aliquet lectus sit. Nulla vehicula ad cursus, nulla ligula leo nec. Magnis ridiculus netus libero mi libero elementum ullamcorper. Nostra class arcu, ex nibh habitant posuere. Nostra curae finibus urna est maecenas.
-
 int calculate() {
   return 6 * 7;
 }
